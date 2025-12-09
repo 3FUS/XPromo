6723da400e159bf478820a6928bfe666eff4842b
FU <1149124450@qq.com>
2025-12-09 10:19:51 +0700

    update win setup25.0.1.8

--- utils/translator.py
+++ utils/translator.py
@@ -1,24 +1,22 @@
 
 import yaml
-import os
 
+import os
+import sys
 
 def load_translations():
     # 使用 getattr 和 getattr 来安全地确定基础路径
     if getattr(sys, 'frozen', False):
-        # 如果是打包后的可执行文件
-        base_path = os.path.dirname(sys.executable)
+        config_path = os.path.join(os.path.dirname(sys.executable), 'config', 'translations_msg.py')
     else:
-        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'translations_msg.py')
 
-    config_path = os.path.join(base_path, 'config', 'translations_msg.yaml')
-
-    # 确保文件存在
     if not os.path.exists(config_path):
         raise FileNotFoundError(f"Translation file not found at: {config_path}")
 
     with open(config_path, 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
+
 
 MESSAGES = load_translations()
 
